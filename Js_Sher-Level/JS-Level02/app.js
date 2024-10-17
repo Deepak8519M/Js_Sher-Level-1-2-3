c4562b8f2a528d31f25125ee078a860725b9e557
Deepak8519M <157715193+Deepak8519M@users.noreply.github.com>
2024-10-17 15:59:53 +0530

    Add files via upload

---
+++ Js_Sher-Level/JS-Level02/app.js
@@ -0,0 +1,76 @@
+// ------------------------------------------
+// var obj1 = {
+//   firstName: "Ajay",
+//   lastName: "Shukla",
+//   age: 32,
+// };
+
+// var obj2 = { ...obj1 };
+
+// obj2.firstName = "Virat";
+// ------------------------------------------
+// var a = [1, 2, 3, 4, 5];
+// var b = [...a];
+
+// b[0] = 10;
+
+// console.log(a);
+// console.log(b);
+// ------------------------------------------
+
+// var obj1 = {
+//   firstName: "Ajay Shukla",
+// };
+
+// if (obj1) {
+//   console.log("HI");
+// } else {
+//   console.log("Bye");
+// }
+// ------------------------------------------
+
+// var a = [1, 2, 3, 4, 5, 6, 6, 7, 7, 4, 5, 567, 8];
+
+// a.forEach(function (val) {
+// console.log(val + 2);   cant change the original value of array
+// });
+
+// ------------------------------------------
+
+// var obj1 = {
+//   firstName: "Ajay",
+//   lastName: "Shukla",
+//   age: 32,
+// };
+// ------------------------------------------
+// for (var key in obj1) {
+//   console.log(key, obj1[key]);
+// }
+
+// var a = [1, 2, 3, 4, 56, 6];
+
+// for (var val in a) {
+//   console.log(`${val}: ${a[val]}`);
+// }
+// ------------------------------------------
+
+// function a(b) {
+//   b(1, 2);
+// }
+
+// a(function (a, b) {
+//   console.log(a + b);
+// });
+// ------------------------------------------
+// b = function () {
+//   console.log("Heheehh");
+// };
+
+// b();
+// ------------------------------------------
+
+// var a = [12, 34, 56, 67, 8, 9];
+
+//  a[-1] = 32;
+
+// var b = a.slice(0, 2);
